4517612719ec05826420e131c502101303f588b9
williamwsf333 <williamwsf333@yahoo.com>
2017-10-02 15:30:54 -0400

    Initial commit: Create Account class and unit tests

---
+++ code/Account.java
@@ -0,0 +1,69 @@
+
+/**
+ * @author      Group 3
+ * @instructor  Prof Ewa Syta
+ * @date		Oct 1, 2017 
+ * @title       CS 310 Software Design Group Project
+ * @Description This class is implements user account
+ *              
+ *
+ */
+public class Account {
+	private String user_ID, username, password, app_name;
+	
+	// public constructor
+	public Account(String user_ID, String username, String password, String app_name) {
+		this.user_ID = user_ID;
+		this.username = username;
+		this.password = password;
+		this.app_name = app_name;
+	}
+	
+	// methods to set user_ID
+	public void setUser_ID(String entry) {
+		this.user_ID = entry;
+	}
+
+	// methods to set username
+	public void setUsername(String entry) {
+		this.username = entry;
+	}
+
+	// methods to set password
+	public void setPassword(String entry) {
+		this.password = entry;
+	}
+
+	// methods to set app_name
+	public void setApp_name(String entry) {
+		this.app_name = entry;
+	}
+
+	// method to get user_ID
+	public String getUser_ID() {
+		return this.user_ID;
+	}
+	
+	// methods to get username
+	public String getUsername() {
+		return this.username;
+	}
+	
+	// methods to get password
+	public String getPassword() {
+		return this.password;
+	}
+	
+	// methods to get app_name
+	public String getApp_name() {
+		return this.app_name;
+	}
+
+	public String toString(){
+		return ("This is the account for user_ID: " + getUser_ID() 
+			+ ", with username: " + getUsername() + ", password: " + getPassword() + 
+			" and for web/application: " + getApp_name() + ".");
+	}
+
+
+}
