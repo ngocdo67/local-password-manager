dde4eec035cd7e6ece02fb49a0a59e4b4c8b999a
Ha Ngan Tran <ha@84788b7f6845.student.trincoll.edu>
2017-10-05 15:35:16 -0400

    Add the user class I created

--- code/User.java
+++ code/User.java
@@ -1,31 +1,27 @@
 
 /**
+ * The User program creates a user for the application along with its username and password
  * @author      Group 3
- * @instructor  Prof Ewa Syta
- * @date	
- * @title       CS 310 Software Design Group Project
- * @Description This class implements the user account
- *              
+ * @version 	1.0	
+ * @since 		2017-10-05             
  */
 
-import java.util.HashMap;
+import java.util.*;
 
 public class User {
 	private String userID, keyPass;
 	private Account account;
+	private Map<Integer, String> manager = new HashMap<Integer, String>();
 
+	/**
+	 * Construct a new User instance
+	 *
+	 * @param userID is the username for the application
+	 * @param keyPass is the password for the application
+	 *
+	 */
 	public User(String userID, String keyPass) {
 		this.userID = userID;
 		this.keyPass = keyPass;
-	}
-
-	HashMap<Integer, String> manager = new HashMap<Integer, String>();
-
-	public void setKeyPass(String keyPass) {
-		this.keyPass = keyPass;
-	}
-
-	public String getKeyPass () {
-		return this.keyPass;
 	}
 }
