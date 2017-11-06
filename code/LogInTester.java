d9e23edbb3eb00ab80f4a15cdaccc5aff3036950
williamwsf333 <williamwsf333@yahoo.com>
2017-11-06 16:29:13 -0500

    removed xml files and reformat LogInTester

--- code/LogInTester.java
+++ code/LogInTester.java
@@ -8,7 +8,7 @@
 
 public class LogInTester {
 
-    public static void main (String[] args) {
+    public static void main(String[] args) {
 
         LogIn logInTestCase = new LogIn();
         if (logInTestCase.verifyNameAndPassword("Shufan", "CS310")) {
