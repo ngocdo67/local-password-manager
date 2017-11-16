11528adb3a2b77ce74ab9866ce4fd699f2e9e337
nganha1897 <nganha1897@gmail.com>
2017-11-16 16:58:53 -0500

    Fixed PasswordProtectorGUI

--- code/main/User.java
+++ code/main/User.java
@@ -23,7 +23,7 @@ import java.util.logging.Logger;
  * @since 2017-10-12
  */
 public class User {
-    private static final String FILE_NAME = "/Users/ha/Documents/cs310/CPSC310-F17-Group3/code/resources/user.txt";
+    private static final String FILE_NAME = "code/resources/user.txt";
     private String userLogIn, keyPass;
     private HashMap<Integer, EncryptedAccount> manager = new HashMap<>();
     private UserFileConverter userFileConverter;
